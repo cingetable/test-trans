d89effa9cadc7d1c9c02b1a4dc50bb95b09c8fdf
valentin_bazdukov <bazdukovvalentin@gmail.com>
2023-04-05 16:46:24 +1000

    refactor(application): Почистить неиспользуемый код

--- translator.h
+++ translator.h
@@ -11,19 +11,9 @@ class Translator : public QObject
 public:
     explicit Translator(QQmlApplicationEngine* engine);
     Q_INVOKABLE void selectLanguage(QString language);
-
-signals:
-    void languageChanged();
-
 private:
     QQmlApplicationEngine *_engine;
     QTranslator *_translator;
-
-    std::map<std::string, std::string> languageFilepath {
-        {"English", ":/main_en.qm"},
-        {"Русский", ":/main_ru.qm"},
-        {"日本語", ":/main_jp.qm"},
-    };
 };
 
 #endif // TRANSLATOR_H
